1933416c1f392ead2379454fb46b024959bd2054
Javier <javier.velez12@upr.edu>
2021-03-21 11:10:11 -0400

    forgot to include models.py from base app

---
+++ scannvote/base/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Student(models.Model):
+    faculty_choices = {
+        ('ingenieria', 'ingeniería'),
+        ('biologia', 'biología'),
+        ('humanidades', 'humanidades'),
+    }
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=9, blank=False)
+    faculty = models.CharField(max_length=50, choices=faculty_choices)
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
+    instance.student.save()
+
+
+
