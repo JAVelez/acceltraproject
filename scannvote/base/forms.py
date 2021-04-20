e4c303fc82963474af731ceb2dc98422e9d97851
Javier <javier.velez12@upr.edu>
2021-04-20 11:24:43 -0400

    added new username and password validations and tests

--- scannvote/base/forms.py
+++ scannvote/base/forms.py
@@ -7,6 +7,17 @@ from .models import Student
 
 # validator to parse student id
 numeric = RegexValidator(r'\d{9}', 'Only digit characters.')
+special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '=']
+
+user_error_messages = {'un_lt': 'Username length must be greater than or equal to 8.',
+                       'un_gt': 'Username length must be less than or equal to 16.',
+                       'pw_lt': 'Password length must be greater than or equal to 8.',
+                       'pw_gt': 'Password length must be less than or equal to 24.',
+                       'pw_upper': 'Password must contain an uppercase letter.',
+                       'pw_lower': 'Password must contain a lowercase letter.',
+                       'pw_digit': 'Password must contain a digit.',
+                       'pw_sc': 'Password must contain a special character.'
+                       }
 
 
 class SignUpForm(UserCreationForm):
@@ -36,10 +47,61 @@ class SignUpForm(UserCreationForm):
             )
         return student_id
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if len(username) < 8:
+            raise ValidationError(user_error_messages['un_lt'],)
+
+        if len(username) > 16:
+            raise ValidationError(user_error_messages['un_gt'],)
+
+        return username
+
+    def clean_password1(self):
+        pw = self.data['password1']
+
+        if len(pw) < 8:
+            raise ValidationError(user_error_messages['pw_lt'],)
+
+        if len(pw) > 24:
+            raise ValidationError(user_error_messages['pw_gt'],)
+
+        if not hasUpper(pw):
+            raise ValidationError(user_error_messages['pw_upper'],)
+
+        if not hasLower(pw):
+            raise ValidationError(user_error_messages['pw_lower'],)
+
+        if not hasDigit(pw):
+            raise ValidationError(user_error_messages['pw_digit'],)
+
+        if not hasSpecialchar(pw):
+            raise ValidationError(user_error_messages['pw_sc'],)
+        return pw
+
     class Meta:
         model = User
         fields = ('username', 'student_id', 'password1', 'password2',)
 
+
+def hasUpper(inputString):
+    return any(char.isupper() for char in inputString)
+
+
+def hasLower(inputString):
+    return any(char.islower() for char in inputString)
+
+
+def hasDigit(inputString):
+    return any(char.isdigit() for char in inputString)
+
+
+def hasSpecialchar(inputString):
+    for s in special_characters:
+        if inputString.__contains__(s):
+            return True
+
+    return False
 
 class LoginForm(AuthenticationForm):
     pass
--- scannvote/base/tests.py
+++ scannvote/base/tests.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from .forms import user_error_messages
 
 # url's
 signup_url = '/signup/'
@@ -19,6 +20,16 @@ signup_fail_student_id_lt_length = 4
 signup_fail_student_id_gt_length = 5
 signup_fail_student_id_non_numeric = 6
 
+signup_fail_pw_lt = 7
+signup_fail_pw_gt = 8
+signup_fail_pw_no_upper = 9
+signup_fail_pw_no_lower = 10
+signup_fail_pw_no_digit = 11
+signup_fail_pw_no_sc = 12
+
+signup_fail_un_lt = 13
+signup_fail_un_gt = 14
+
 # login codes
 login_good = 0
 login_bad_pw = 1
@@ -28,7 +39,7 @@ login_bad_both = 3
 # test data
 good_username = 'gooduser1'
 good_student_id = '000000000'
-good_pw = 'GoodPW12'
+good_pw = 'GoodPW!@12'
 
 good_admin = 'goodadmin1'
 good_admin_id = '000000001'
@@ -36,8 +47,16 @@ good_admin_id = '000000001'
 lt_student_id = '12345678'
 gt_student_id = '1234567890'
 char_student_id = '12345678a'
-bad_pw = 'password'
-bad_username = 'baduser'
+
+pw_lt = 'Paswo@1'
+pw_gt = 'Paasfga#21ffasdDsojkf$ashsuDfgh'
+pw_no_upper = 'noupperpwforl!f3'
+pw_no_lower = 'NOLOWERPWFORL!F3'
+pw_no_digit = 'Nodigitpwforl!fe'
+pw_no_sc = 'Nodigitpwforlif3'
+
+username_lt = 'baduser'
+username_gt = 'ThisIsMyUsernameFromNowOnAndItsALongOne'
 
 # represents good user registration
 good_student = {'username': good_username, 'student_id': good_student_id, 'password1': good_pw, 'password2': good_pw}
@@ -46,9 +65,9 @@ good_student = {'username': good_username, 'student_id': good_student_id, 'passw
 good_admin = {'username': good_admin, 'student_id': good_admin_id, 'password1': good_pw, 'password2': good_pw}
 
 # represent bad user registrations
-student_mismatch_pw = {'username': good_username, 'student_id': good_student_id, 'password1': good_pw, 'password2': bad_pw}
+student_mismatch_pw = {'username': good_username, 'student_id': good_student_id, 'password1': good_pw, 'password2': pw_lt}
 
-student_weak_pw = {'username': good_username, 'student_id': good_student_id, 'password1': bad_pw, 'password2': bad_pw}
+student_weak_pw = {'username': good_username, 'student_id': good_student_id, 'password1': pw_lt, 'password2': pw_lt}
 
 student_id_empty = {'username': good_username, 'student_id': '', 'password1': good_pw, 'password2': good_pw}
 
@@ -58,17 +77,36 @@ student_id_gt = {'username': good_username, 'student_id': gt_student_id, 'passwo
 
 student_id_char = {'username': good_username, 'student_id': char_student_id, 'password1': good_pw, 'password2': good_pw}
 
+# signup username validations
+student_username_lt = {'username': username_lt, 'student_id': good_student_id, 'password1': good_pw, 'password2': good_pw}
+student_username_gt = {'username': username_gt, 'student_id': good_student_id, 'password1': good_pw, 'password2': good_pw}
+
+# signup pw validations
+student_pw_lt = {'username': good_username, 'student_id': good_student_id, 'password1': pw_lt, 'password2': pw_lt}
+student_pw_gt = {'username': good_username, 'student_id': good_student_id, 'password1': pw_gt, 'password2': pw_gt}
+
+student_pw_no_upper = {'username': good_username, 'student_id': good_student_id, 'password1': pw_no_upper, 'password2': pw_no_upper}
+student_pw_no_lower = {'username': good_username, 'student_id': good_student_id, 'password1': pw_no_lower, 'password2': pw_no_lower}
+
+student_pw_no_digit = {'username': good_username, 'student_id': good_student_id, 'password1': pw_no_digit, 'password2': pw_no_digit}
+student_pw_no_sc = {'username': good_username, 'student_id': good_student_id, 'password1': pw_no_sc, 'password2': pw_no_sc}
+
+
+
+
 # represent bad user logins
 
-bad_student_login_pw = {'username': good_username, 'password1': bad_pw}
+bad_student_login_pw = {'username': good_username, 'password1': pw_lt}
 
-bad_student_login_username = {'username': bad_pw, 'password1': good_pw}
+bad_student_login_username = {'username': pw_lt, 'password1': good_pw}
 
-bad_student_login_both = {'username': bad_username, 'password1': bad_pw}
+bad_student_login_both = {'username': username_lt, 'password1': pw_lt}
 
 # html items
 error_mismatch_pw = '/html/body/main/div/form/p[6]'
 error_common_pw = error_mismatch_pw
+error_new_pw_validations = '/html/body/main/div/form/p[4]'
+error_new_user_validations = '/html/body/main/div/form/p[2]'
 error_length_sid = '/html/body/main/div/form/p[3]'
 error_char_sid = error_length_sid
 home_username = '/html/body/main/div/h2'
@@ -93,8 +131,25 @@ class StudentTestCases(StaticLiveServerTestCase):
             form = good_student
         elif code == signup_fail_mismatch_pw:
             form = student_mismatch_pw
-        elif code == signup_fail_weak_password:
-            form = student_weak_pw
+
+        elif code == signup_fail_un_lt:
+            form = student_username_lt
+        elif code == signup_fail_un_gt:
+            form = student_username_gt
+
+        elif code == signup_fail_pw_lt:
+            form = student_pw_lt
+        elif code == signup_fail_pw_gt:
+            form = student_pw_gt
+        elif code == signup_fail_pw_no_upper:
+            form = student_pw_no_upper
+        elif code == signup_fail_pw_no_lower:
+            form = student_pw_no_lower
+        elif code == signup_fail_pw_no_digit:
+            form = student_pw_no_digit
+        elif code == signup_fail_pw_no_sc:
+            form = student_pw_no_sc
+
         elif code == signup_fail_student_id_empty:
             form = student_id_empty
         elif code == signup_fail_student_id_lt_length:
@@ -136,14 +191,49 @@ class StudentTestCases(StaticLiveServerTestCase):
         self.assertEqual(self.browser.find_element_by_xpath(error_mismatch_pw).text, 'Passwords do not match.')
         self.signup_form_clear()
 
-        # weak password validation error
-        self.signup_form_fill(signup_fail_weak_password)
-        self.assertEqual(self.browser.find_element_by_xpath(error_common_pw).text, 'This password is too common.')
+        # less than length username validation error
+        self.signup_form_fill(signup_fail_un_lt)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_user_validations).text, user_error_messages['un_lt'])
+        self.signup_form_clear()
+
+        # greater than length username validation error
+        self.signup_form_fill(signup_fail_un_gt)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_user_validations).text, user_error_messages['un_gt'])
+        self.signup_form_clear()
+
+        # less than length password validation error
+        self.signup_form_fill(signup_fail_pw_lt)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_lt'])
+        self.signup_form_clear()
+
+        # greater than length password validation error
+        self.signup_form_fill(signup_fail_pw_gt)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_gt'])
+        self.signup_form_clear()
+
+        # no upper password validation error
+        self.signup_form_fill(signup_fail_pw_no_upper)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_upper'])
+        self.signup_form_clear()
+
+        # no lower password validation error
+        self.signup_form_fill(signup_fail_pw_no_lower)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_lower'])
+        self.signup_form_clear()
+
+        # no digit password validation error
+        self.signup_form_fill(signup_fail_pw_no_digit)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_digit'])
+        self.signup_form_clear()
+
+        # no special character password validation error
+        self.signup_form_fill(signup_fail_pw_no_sc)
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_sc'])
         self.signup_form_clear()
 
         # blank student id when submitting form error
         self.signup_form_fill(signup_fail_student_id_empty)
-        self.assertEqual(self.browser.find_element_by_xpath(error_common_pw).text, 'This password is too common.')
+        self.assertEqual(self.browser.find_element_by_xpath(error_new_pw_validations).text, user_error_messages['pw_sc'])
         self.signup_form_clear()
 
         # student id less than 9 digits long validation error
